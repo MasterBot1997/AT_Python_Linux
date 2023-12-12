4319dafea5a827bacaca514e93a9671ee076b5b1
Ivan Belyaev <bel3187228@yandex.ru>
2023-12-12 19:07:02 +0300

    Добавлена функция user_name
    Функция возвращает имя пользователя linux у которого в дириктории выполняется тест файл, для универсальности кода

--- HW_2/task_1.py
+++ HW_2/task_1.py
@@ -13,12 +13,16 @@ def func(command, text):
 def func_del_arh(command):
     subprocess.run(command, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
 
+def user_name():
+    result = subprocess.run('pwd', shell=True, stdout=subprocess.PIPE, encoding='utf-8').stdout.split('/')[2]
+    return result
 
 if __name__ == '__main__':
-    val_1 = 'cd ~/shared/AT_Python_Linux/HW_2/test_step_zip/fold_1; 7z a ../fold_2/arch_fold_1'
+    # val_1 = 'cd ~/shared/AT_Python_Linux/HW_2/test_step_zip/fold_1; 7z a ../fold_2/arch_fold_1'
     # val_1 = 'cd ~/shared/AT_Python_Linux/HW_2/test_step_zip/fold_2; 7z h'
     # val_1 = 'cat /etc' # Проверка на ложь
-    val_2 = 'Everything is Ok'
+    # val_2 = 'Everything is Ok'
     # val_2 = '1 file, 265 bytes (1 KiB)'
-    print(func(val_1, val_2))
-    # func_del('cd ~/shared/AT_Python_Linux/HW_2/test_step_zip/fold_2; rm ./arch_fold_1.7z')
+    # print(func(val_1, val_2))
+    # func_del_arh('cd ~/shared/AT_Python_Linux/HW_2/test_step_zip/fold_2; rm ./arch_fold_1.7z')
+    print(user_name())
